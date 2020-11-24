82b128a5a7ae1cae153bfa8e4f24324c331f5a58
Aaron Payne <aaron.payne@my.uwi.edu>
2020-11-24 03:38:54 -0400

    AppBar Removed and status bar color set to update with color change. Fixed: Original EnterAppointmentDetails.java linked to Home Screen.
    UserSettings.java added to image button on the home screen.

--- app/src/main/java/com/example/HSUAppProject/HomeScreen.java
+++ app/src/main/java/com/example/HSUAppProject/HomeScreen.java
@@ -1,7 +1,14 @@
 package com.example.HSUAppProject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.example.HSUAppProject.ui.UserSettings;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +18,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class HomeScreen extends AppCompatActivity {
+    ImageButton userButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,27 @@ public class HomeScreen extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.ic_home, R.id.ic_notification, R.id.addappt, R.id.ic_prescription, R.id.ic_medrecord)
-                .build();
+        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        //        R.id.ic_home, R.id.ic_notification, R.id.addappt, R.id.ic_prescription, R.id.ic_medrecord)
+        //.build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        userButton = (ImageButton) findViewById(R.id.imageButton);
+        userButton.setOnClickListener(new ImageButton_Clicker());
     }
 
+    class ImageButton_Clicker implements ImageButton.OnClickListener {
+        @Override
+        public void onClick(View v) {
+
+            if (v == userButton) {
+                Context context = getApplicationContext();
+                Intent intent = new Intent(context, UserSettings.class);
+
+                startActivity(intent);
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/HSUAppProject/ui/dashboard/SelectTypeFragment.java
+++ app/src/main/java/com/example/HSUAppProject/ui/dashboard/SelectTypeFragment.java
@@ -1,9 +1,12 @@
 package com.example.HSUAppProject.ui.dashboard;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,9 +15,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.HSUAppProject.EnterAppointmentDetails;
+import com.example.HSUAppProject.HomeScreen;
 import com.example.HSUAppProject.R;
+import com.example.HSUAppProject.ui.UserSettings;
 
 public class SelectTypeFragment extends Fragment {
+    private Button newButton;
 
     private SelectTypeViewModel selectTypeViewModel;
 
@@ -24,12 +31,32 @@ public class SelectTypeFragment extends Fragment {
                 new ViewModelProvider(this).get(SelectTypeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_select_type, container, false);
         final TextView textView = root.findViewById(R.id.text_dashboard);
+        
         selectTypeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
+        newButton = (Button) root.findViewById(R.id.button2);
+        newButton.setOnClickListener(new Button_Clicker());
+
+
         return root;
+    }
+
+    class Button_Clicker implements Button.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (v == newButton) {
+
+                Intent intent = new Intent(getActivity(), EnterAppointmentDetails.class);
+                startActivity(intent);
+
+            }
+
+        }
+
     }
 }
